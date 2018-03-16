31b6cd5592067d7b2bb156dce88c2603ac1c9263
pankaju <pankaju@winsoftech.com>
2018-03-16 06:31:04 -0700

    Updated

--- TrilloChatApp/static/js/SideBarC.js
+++ TrilloChatApp/static/js/SideBarC.js
@@ -2,18 +2,18 @@
 
 (function() {
 
-  TrilloChatApp.SideBarC = Trillo.inherits(Trillo.Controller, function(viewSpec) {
-    Trillo.Controller.call(this, viewSpec);
+  TrilloChatApp.SideBarC = Trillo.inherits(Shared.SharedC, function(viewSpec) {
+    Shared.SharedC.call(this, viewSpec);
   });
 
   var SideBarC = TrilloChatApp.SideBarC.prototype;
-  var Controller = Trillo.Controller.prototype;
+  var SharedC = Shared.SharedC.prototype;
 
   SideBarC.handleAction = function(actionName, selectedObj, $e, targetController) {
-    if (actionName === "about") {
-      $("#about").modal("show");
-      return true;
-    }
-    return Controller.handleAction.call(this, actionName, selectedObj, $e, targetController);
+    return SharedC.handleAction.call(this, actionName, selectedObj, $e, targetController);
+  };
+  
+  SideBarC.postViewShown = function(view) {
+    SharedC.postViewShown.call(this, view);
   };
 })();
